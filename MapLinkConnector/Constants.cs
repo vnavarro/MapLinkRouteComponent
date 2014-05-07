fb8871ac07480b9f507556494370c7b46c35a006
vitor.navarro <vitor.navarro87@gmail.com>
2014-05-07 20:28:04 -0300

    Use maplink route calculation
    * WARNING: THE TEST PROJECT ISNT RUNNING ON MY VS2010!!!

---
+++ MapLinkConnector/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MapLinkConnector
+{
+    public class Constants
+    {
+        public const int ROUTE_TYPE_STANDARD_FASTEST = 0;
+        public const int ROUTE_TYPE_AVOID_TRAFFIC = 23;
+    }
+}
--- MapLinkConnector/RouteCalculator.cs
+++ MapLinkConnector/RouteCalculator.cs
@@ -3,14 +3,90 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MapLinkConnector.MaplinkV3_AddressFinder;
-using Newtonsoft.Json.Linq;
+using MapLinkConnector.MaplinkV3_Route;
 
 namespace MapLinkConnector
 {
     public class RouteCalculator
     {
-        public void calculate(dynamic address)
-        {           
+        private AddressOptions findAddressOptions;
+        private List<AddressLocation> locations;
+        private string token;
+
+        public RouteCalculator()
+        {
+            this.findAddressOptions = new AddressOptions
+            {
+                usePhonetic = true,
+                searchType = 2,
+                resultRange = new ResultRange { pageIndex = 1, recordsPerPage = 10 }
+            };
+            this.token = System.Configuration.ConfigurationManager.AppSettings["MapLinkToken"];
+            this.locations = new List<AddressLocation>();
+        }
+
+        public RouteOptions DefaultRouteOptions(int routeType)
+        {
+            return new RouteOptions
+            {
+                language = "portugues",
+                routeDetails = new RouteDetails { descriptionType = 0, routeType = routeType, optimizeRoute = true },                
+                vehicle = this.DefaultPopularVehicle()
+            };
+        }
+
+        private Vehicle DefaultPopularVehicle(){
+            return new Vehicle
+            {
+                tankCapacity = 20,
+                averageConsumption = 9,
+                fuelPrice = 3,
+                averageSpeed = 60,
+                tollFeeCat = 2
+            };
+        }
+
+
+        public void FindAdresses(string addressList)
+        {
+            IList<Address> addresses = new JsonParser().ParseListOfAddress(addressList);
+
+            var soapClient = new AddressFinderSoapClient();
+
+            foreach (Address address in addresses)
+            {
+                var findAddressResponse = soapClient
+                    .findAddress(address, this.findAddressOptions, this.token);
+
+                AddressLocation location = findAddressResponse.addressLocation.ToList().First();
+                this.locations.Add(location);
+            }
+        }
+
+        public RouteStop[] GenerateRoutes()
+        {
+            List<RouteStop> routes = new List<RouteStop>();
+            this.locations.ForEach( location => 
+                routes.Add(new RouteStop
+                {
+                    description = location.address.street + location.address.houseNumber,
+                    point = new MapLinkConnector.MaplinkV3_Route.Point { x = location.point.x, y = location.point.y }
+                })
+            );
+                                    
+            return routes.ToArray();                        
+        }        
+
+        public RouteTotals Calculate(int routeType)
+        {
+            RouteStop[] routes = this.GenerateRoutes();
+
+            using (var routeSoapClient = new RouteSoapClient())
+            {
+                var getRouteTotalsResponse = routeSoapClient
+                    .getRouteTotals(routes, this.DefaultRouteOptions(routeType), token);                
+                return getRouteTotalsResponse;
+            }
         }
     }
 }
